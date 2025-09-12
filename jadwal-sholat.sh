39a27cdfbaa6fe576089356eb2a6f0cb2658aada
g3n1u5d <118767452+g3n1u5d@users.noreply.github.com>
2025-09-12 23:14:27 +0700

    Update jadwal-sholat.sh

--- jadwal-sholat.sh
+++ jadwal-sholat.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# Ambil data sholat
-#untuk kota selain kota palembang pada link url id diganti sesuai kode kota anda
-data=$(curl -s -A "Mozilla/5.0" "https://jadwalsholat.org/jadwal-sholat/monthly.php?id=177" \
-| grep "<td><b>$(date +%d)</b></td>" \
-| sed -E 's/<tr[^>]*>|<\/tr>//g' \
-| sed -E 's/<[^>]+>/ /g' \
-| awk '{$1=""; print $0}' \
-| xargs)
+# Lokasi folder tempat skrip ini berada
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+tanggal=$(date +%Y-%m-%d)
+cache_file="$script_dir/jadwalsholat-$tanggal.html"
+
+# Cek apakah file cache sudah ada
+if [ ! -f "$cache_file" ]; then
+    echo "update jadwal sholat tanggal $tanggal..."
+    curl -s -A "Mozilla/5.0" "https://jadwalsholat.org/jadwal-sholat/monthly.php?id=177" > "$cache_file"
+fi
+
+# Ambil data dari file cache
+data=$(grep "<td><b>$(date +%d)</b></td>" "$cache_file" \
+    | sed -E 's/<tr[^>]*>|<\/tr>//g' \
+    | sed -E 's/<[^>]+>/ /g' \
+    | awk '{$1=""; print $0}' \
+    | xargs)
 
 # Bagi ke dalam array
 read -ra times <<< "$data"
 
-# Tampilkan dengan format rapi
+# Tampilkan hasil
 printf "\nJadwal Sholat Hari Ini (Tanggal %s):\n\n" "$(date +%d-%m-%Y)"
 printf "%-8s %-8s %-8s %-8s %-8s %-8s %-8s %-8s\n" \
   "Imsyak" "Subuh" "Terbit" "Dhuha" "Dzuhur" "Ashar" "Maghrib" "Isya"
