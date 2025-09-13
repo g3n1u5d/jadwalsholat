b1781a4edccd880ec8d0e58d22d836be3bc54c22
g3n1u5d <118767452+g3n1u5d@users.noreply.github.com>
2025-09-13 11:58:42 +0700

    Berbagai tampilan yang cocok disandingkan dengan conky

--- jadwalsholat.sh
+++ jadwalsholat.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Lokasi folder tempat skrip ini berada
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+script_dir="/tmp"
 tanggal=$(date +%Y-%m-%d)
 cache_file="$script_dir/jadwalsholat-$tanggal.html"
 
@@ -21,9 +22,23 @@ data=$(grep "<td><b>$(date +%d)</b></td>" "$cache_file" \
 # Bagi ke dalam array
 read -ra times <<< "$data"
 
-# Tampilkan hasil
-printf "\nJadwal Sholat Hari Ini (Tanggal %s):\n\n" "$(date +%d-%m-%Y)"
-printf "%-8s %-8s %-8s %-8s %-8s %-8s %-8s %-8s\n" \
-  "Imsyak" "Subuh" "Terbit" "Dhuha" "Dzuhur" "Ashar" "Maghrib" "Isya"
-printf "%-8s %-8s %-8s %-8s %-8s %-8s %-8s %-8s\n" "${times[@]}"
+# Tampilkan hasil horizontal
+# printf "\nJadwal Sholat Hari Ini (Tanggal %s):\n\n" "$(date +%d-%m-%Y)"
+#printf "%-8s %-8s %-8s %-8s %-8s %-8s %-8s %-8s\n" \
+#  "Imsyak" "Subuh" "Terbit" "Dhuha" "Dzuhur" "Ashar" "Maghrib" "Isya"
+#printf "%-8s %-8s %-8s %-8s %-8s %-8s %-8s %-8s\n" "${times[@]}"
 
+# Tampilan secara vertikal 1
+#labels=("Imsyak" "Subuh" "Terbit" "Dhuha" "Dzuhur" "Ashar" "Maghrib" "Isya")
+#for i in "${!labels[@]}"; do
+#  printf "%-8s : %s\n" "${labels[$i]}" "${times[$i]}"
+#done
+
+#tampilan secara vertikal 2
+labels=("Imsak" "Subuh" "Terbit" "Dhuha" "Dzuhur" "Ashar" "Maghrib" "Isya")
+
+# Format 2 kolom: kiri dan kanan
+for i in {0..3}; do
+  printf "%-10s: %-5s\t%-10s: %-5s\n" \
+    "${labels[$i]}" "${times[$i]}" "${labels[$i+4]}" "${times[$i+4]}"
+done
